5bb34e413277974b7b0b8570c1db53e0dae08597
xjg2010go <xjg2010go@163.com>
2021-10-10 13:24:27 +0800

    add webhook 5

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,13 @@ module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'Chrome',
@@ -13,7 +20,14 @@ module.exports = function(config) {
         ]
       }
     },
-    browsers: ['ChromeHeadless','Chrome'],
+    browserNoActivityTimeout: 60000,
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless', 'Chrome'],
     singleRun: true,
+    restartOnFileChange: true
   })
 }
