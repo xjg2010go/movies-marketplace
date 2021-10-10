164afe14f4a9b60233ba9f4f1331c1b4156a2be4
xjg2010go <xjg2010go@163.com>
2021-10-10 13:50:01 +0800

    add webhook 5

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,58 +1,70 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../api.service";
+import { ActivatedRoute } from "@angular/router";
 
 declare var $: any;
 
 @Component({
-  selector: 'app-movie',
-  templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  selector: "app-movie",
+  templateUrl: "./movie.component.html",
+  styleUrls: ["./movie.component.css"],
 })
 export class MovieComponent implements OnInit {
-
   public movie: any = {};
 
-  constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) {
-    this.apiService.getMovie(this.activatedRoute.snapshot.params.id).subscribe(data => {
-       data.actors.shift();
-      this.movie = data;
-    }, err => {
-      this.movie = {};
-    })
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.apiService.getMovie(this.activatedRoute.snapshot.params.id).subscribe(
+      (data) => {
+        data.actors.shift();
+        this.movie = data;
+      },
+      (err) => {
+        this.movie = {};
+      }
+    );
   }
 
-  public addToFav(){
-    this.apiService.addToFavorites(this.movie).subscribe(data => {
-      $.notify({
-        icon: 'la la-bell',
-        title: 'Movie',
-        message: 'has been added to watchlist !',
-      }, {
-          type: 'success',
-          placement: {
-            from: "bottom",
-            align: "right"
+  public addToFav() {
+    this.apiService.addToFavorites(this.movie).subscribe(
+      (data) => {
+        $.notify(
+          {
+            icon: "la la-bell",
+            title: "Movie",
+            message: "has been added to watchlist !",
           },
-          time: 800000,
-        });
-    }, err => {
-      $.notify({
-        icon: 'la la-bell',
-        title: 'Something',
-        message: 'went wrong !',
-      }, {
-          type: 'danger',
-          placement: {
-            from: "bottom",
-            align: "right"
+          {
+            type: "success",
+            placement: {
+              from: "bottom",
+              align: "right",
+            },
+            time: 800000,
+          }
+        );
+      },
+      (err) => {
+        $.notify(
+          {
+            icon: "la la-bell",
+            title: "Something",
+            message: "went wrong !",
           },
-          time: 800000,
-        });
-    });
+          {
+            type: "danger",
+            placement: {
+              from: "bottom",
+              align: "right",
+            },
+            time: 800000,
+          }
+        );
+      }
+    );
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
