ffdf77afa4898b6752f976a13c8e02f69979d8f0
xjg2010go <xjg2010go@163.com>
2021-10-10 13:45:34 +0800

    add webhook 5

--- src/app/movie/movie.component.spec.ts
+++ src/app/movie/movie.component.spec.ts
@@ -1,16 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { MovieComponent } from './movie.component';
+import { MovieComponent } from "./movie.component";
 
-describe('MovieComponent', () => {
+describe("MovieComponent", () => {
   let component: MovieComponent;
   let fixture: ComponentFixture<MovieComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MovieComponent ]
-    })
-    .compileComponents();
+      declarations: [MovieComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('MovieComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
